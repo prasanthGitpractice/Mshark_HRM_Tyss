741825907fbeb68b218ada4a48548f99be093120
prasanthGitpractice <galiprasanth6@gmail.com>
2023-11-30 13:03:15 +0530

    Opening Browser Using Jenkins witg comments

--- src/test/java/com/Practice_Package/SampleTest.java
+++ src/test/java/com/Practice_Package/SampleTest.java
@@ -1,5 +1,7 @@
 package com.Practice_Package;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,10 +12,16 @@ public class SampleTest
 {
 	WebDriver driver;
 	@Test
-	public void m1Test()
+	public void m1Test() throws Throwable
 	{
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in");
+		
+		Thread.sleep(Duration.ofSeconds(5));
+		System.out.println(driver.getTitle());
+		driver.quit();
+		System.out.println("Browser Closed");
+		
 	}
 }
